2e1381f467cba7cfbc1eaf6752e659eec7e7128a
marta6099 <m.garridoj.2018@alumnos.urjc.es>
2021-04-20 20:30:50 +0200

    Ejercicio1

--- P3/L8/canvas-01.js
+++ P3/L8/canvas-01.js
@@ -3,9 +3,9 @@ console.log("Ejecutando JS...");
 const canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-//-- Definir el tamaño del convas
-canvas.width = 300;
-canvas.height = 300;
+//-- Definir el tamaño del canvas
+canvas.width = 480;
+canvas.height = 320;
 
 //Score
 var score = 0;
@@ -26,4 +26,37 @@ function drawVidas() {
 drawScore();
 drawVidas();
 
+//Ladrillos
+var brickRowCount = 5; // cuantos ladrillos de ancho
+var brickColumnCount = 3; //cuants ladrillos de largo
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 100;
+var brickOffsetLeft = 30;
 
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = 0;
+            bricks[c][r].y = 0;
+            ctx.beginPath();
+            ctx.rect(0, 0, brickWidth, brickHeight);
+            ctx.fillStyle = "#804000";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+drawBricks();
