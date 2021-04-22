a73148aa2ff59b9a17ce0d068d185954da8e055e
marta6099 <m.garridoj.2018@alumnos.urjc.es>
2021-04-22 19:14:15 +0200

    Calculadora

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -32,122 +32,145 @@ function main(){
           AC: document.getElementById('AC'),
           DEL: document.getElementById('DEL'),
       }
-    
+    //Para el display
+      var display = {
+        disp: "",
+        update: function (value) {
+          this.disp += value;
+          numeros.display.innerHTML = this.disp;
+        }
+      }
 
     //Boton valor 1
       numeros.boton1.onclick = () => {
       console.log("Click1");
       var valor = document.getElementById("1").value;
-      showdisplay.update(valor);
+      display.update(valor);
       op_elements.num += valor
     }
   
       //Boton valor 2
       numeros.boton2.onclick = () => {
         var valor = document.getElementById("2").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton3.onclick = () => {
         var valor = document.getElementById("3").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton4.onclick = () => {
         var valor = document.getElementById("4").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton5.onclick = () => {
         var valor = document.getElementById("5").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton6.onclick = () => {
         var valor = document.getElementById("6").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton7.onclick = () => {
         var valor = document.getElementById("7").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton8.onclick = () => {
         var valor = document.getElementById("8").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton9.onclick = () => {
         var valor = document.getElementById("9").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
     
       numeros.boton0.onclick = () => {
         var valor = document.getElementById("0").value
-        showdisplay.update(valor);
+        display.update(valor);
         op_elements.num += valor
       }
 
       //Operaciones
-      suma.onclick = () => {
-        console.log("Suma");
+      operaciones.suma.onclick = () => {
         var operation = document.getElementById("+").value;
-        var display = document.getElementById("display");
-        showdisplay = showdisplay + operation;
-        operation_arr.push(num);
-        operation_arr.push(operation);
-        num = "";
-        console.log(operation_arr);
-        display.innerHTML = showdisplay;
+        op_elements.operation_arr.push(op_elements.num);
+        op_elements.operation_arr.push(operation)
+        display.update(operation);
+        op_elements.num = "";
       }
       
-      resta.onclick = () => {
-        console.log("Resta");
+      operaciones.resta.onclick = () => {
+        console.log("rest");
         var operation = document.getElementById("-").value;
-        var display = document.getElementById("display");
-        showdisplay = showdisplay + operation;
-        operation_arr.push(num);
-        operation_arr.push(operation);
-        num = "";
-        console.log(operation_arr);
-        display.innerHTML = showdisplay;
-      }
-      multiplicacion.onclick = () => {
-        console.log("Multiplicación");
-        var operation = document.getElementById("*").value;
-        var display = document.getElementById("display");
-        showdisplay = showdisplay + operation;
-        operation_arr.push(num);
-        operation_arr.push(operation);
-        num = "";
-        console.log(operation_arr);
-        display.innerHTML = showdisplay;
-      }
-      division.onclick = () => {
-        console.log("Multiplicación");
-        var operation = document.getElementById("/").value;
-        var display = document.getElementById("display");
-        showdisplay = showdisplay + operation;
-        operation_arr.push(num);
-        operation_arr.push(operation);
-        num = "";
-        console.log(operation_arr);
-        display.innerHTML = showdisplay;
-      }
-      decimal.onclick = () => {
-        var valor = document.getElementById(".").value;
-        var display = document.getElementById("display");
-        num = num + valor
-        display.innerHTML = (showdisplay = showdisplay + valor);
+        op_elements.operation_arr.push(op_elements.num);
+        op_elements.operation_arr.push(operation);
+        display.update(operation);
+        op_elements.num = "";
       }
 
+      operaciones.multiplicacion.onclick = () => {
+        console.log("mult");
+        var operation = document.getElementById("*").value;
+        op_elements.operation_arr.push(op_elements.num);
+        op_elements.operation_arr.push(operation)
+        display.update(operation);
+        op_elements.num = "";
+      }
+      operaciones.division.onclick = () => {
+        console.log("divi");
+        var operation = document.getElementById("/").value;
+        op_elements.operation_arr.push(op_elements.num);
+        op_elements.operation_arr.push(operation)
+        display.update(operation);
+        op_elements.num = "";
+      }
+      operaciones.decimal.onclick = () => {
+        console.log("decimal");
+        var valor = document.getElementById(".").value;
+        display.update(valor)
+        op_elements.num += valor
+      }
+      operaciones.botonera.onclick = () => {
+        console.log("Igual");
+        op_elements.operation_arr.push(op_elements.num);
+        display.disp = "";
+        op_elements.num = "";
+    
+        operation = op_elements.operation_arr[1];
+        op1 = parseFloat(op_elements.operation_arr[0]);
+        op2 = parseFloat(op_elements.operation_arr[2]);
+    
+        if (operation == "+") {
+          resultado = op1 + op2;
+        } else if (operation == "-") {
+          resultado = op1 - op2;
+        } else if (operation == "*") {
+          resultado = op1 * op2;
+        } else if (operation == "/") {
+          if (op2 == 0) {
+            resultado = "Math error"
+          } else {
+            resultado = op1 / op2;
+          }
+        }
+    
+        console.log(resultado)
+        display.update(resultado);
+        op_elements.operation_arr.splice(0, 3);
+        display.disp = "";
+        }
     }
