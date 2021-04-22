21386dfe6886c6e2f662fb7a9c6a67a64558549e
marta6099 <m.garridoj.2018@alumnos.urjc.es>
2021-04-22 18:30:34 +0200

    Terminado la funcion de onclick para los num

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -28,4 +28,80 @@ function main(){
         var DEL= document.getElementById('DEL');
     }
 
+    //Boton valor 0
+    boton0.onclick = () => {
+      var valor = document.getElementById("0").value;
+      var display = document.getElementById("display");
+      num = num + valor
+      display.innerHTML = (showdisplay = showdisplay + valor);
+    }
+
+    //Boton valor 1
+    boton1.onclick = () => {
+      console.log("Click1");
+      var valor = document.getElementById("1").value;
+      var display = document.getElementById("display");
+      num = num + valor
+      display.innerHTML = (showdisplay = showdisplay + valor);
+    }
   
+      //Boton valor 2
+      boton2.onclick = () => {
+        console.log("Click2");
+        var valor = document.getElementById("2").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+  
+      //Boton valor 3
+      boton3.onclick = () => {
+        console.log("Click3");
+        var valor = document.getElementById("3").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+
+      boton4.onclick = () => {
+        var valor = document.getElementById("4").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
+      boton5.onclick = () => {
+        var valor = document.getElementById("5").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
+      boton6.onclick = () => {
+        var valor = document.getElementById("6").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
+      boton7.onclick = () => {
+        var valor = document.getElementById("7").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
+      boton8.onclick = () => {
+        var valor = document.getElementById("8").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
+      boton9.onclick = () => {
+        var valor = document.getElementById("9").value;
+        var display = document.getElementById("display");
+        num = num + valor
+        display.innerHTML = (showdisplay = showdisplay + valor);
+      }
+    
