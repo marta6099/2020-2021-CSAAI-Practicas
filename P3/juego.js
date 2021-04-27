05e12f0082285c7851ecdce3248361aff06c1750
marta6099 <m.garridoj.2018@alumnos.urjc.es>
2021-04-27 12:13:26 +0200

    Implementado el game over

--- P3/juego.js
+++ P3/juego.js
@@ -7,48 +7,21 @@ var ctx = canvas.getContext("2d");
 canvas.width = 580;
 canvas.height = 520;
 
-//
-var dx = 2;
-var dy = -2;
+//Para la colision
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-/* //Definimos una bola
-ctx.beginPath();
-    //-- Dibujar un circulo: coordenadas x,y del centro
-    //-- Radio, Angulo inicial y angulo final
-    ctx.arc(250, 250, 10, 0, 2 * Math.PI);
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 3;
-    ctx.fillStyle = 'white';
-
-    //-- Dibujar el trazo
-    ctx.stroke()
-
-    //-- Dibujar el relleno
-    ctx.fill()
-    
-ctx.closePath() */
-
-//Ladrillos
-var brickRowCount = 5; // cuantas filas de ladrillos
-var brickColumnCount = 9; //cuantas columnas de ladrillos
-var brickWidth = 55; //Ancho del ladrillo
-var brickHeight = 20; //alto del ladrillos
-var brickPadding = 1; //huecos entre ladrillos
-var brickOffsetTop = 50; //margen superior
-var brickOffsetLeft = 30; //margen inferior
-
-var bricks = [];
-for(c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
-    for(r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
-    }
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fill();
+    ctx.closePath();
 }
-
-//Funcion para mover la raqueta con teclado
+//DEFINIMOS TECLADO
 var rightPressed = false;
 var leftPressed = false;
 document.addEventListener("keydown", keyDownHandler, false);
@@ -69,15 +42,8 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-//Definimos la bola
 
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#ffffff";
-    ctx.fill();
-    ctx.closePath();
-}
+
 
 //Definimos una raqueta
 var paddleHeight = 10;
@@ -90,6 +56,22 @@ function drawPaddle() {
     ctx.fillStyle = "#FFFFFF";
     ctx.fill();
     ctx.closePath();
+}
+//Ladrillos
+var brickRowCount = 5; // cuantas filas de ladrillos
+var brickColumnCount = 9; //cuantas columnas de ladrillos
+var brickWidth = 55; //Ancho del ladrillo
+var brickHeight = 20; //alto del ladrillos
+var brickPadding = 1; //huecos entre ladrillos
+var brickOffsetTop = 50; //margen superior
+var brickOffsetLeft = 30; //margen inferior
+
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
 }
 
 function drawBricks() {
@@ -104,10 +86,9 @@ function drawBricks() {
             ctx.fillStyle = "#804000";
             ctx.fill();
             ctx.closePath();
-            }
         }
     }
-
+}
 //Score
 var score = 0;
 function drawScore() {
@@ -116,6 +97,7 @@ function drawScore() {
     ctx.fillText("Score: "+score, 8, 20);
 }
 
+
 //Vidas
 var vidas = 3;
 function drawVidas() {
@@ -123,14 +105,11 @@ function drawVidas() {
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Vidas: "+vidas, canvas.width-65, 20);
 }
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBricks();
     drawBall();
     drawPaddle();
-    drawScore();
-    drawLives();
+    drawBricks();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -143,20 +122,11 @@ function draw() {
             dy = -dy;
         }
         else {
-            lives--;
-            if(!lives) {
-                alert("GAME OVER");
-                document.location.reload();
-            }
-            else {
-                x = canvas.width/2;
-                y = canvas.height-30;
-                dx = 3;
-                dy = -3;
-                paddleX = (canvas.width-paddleWidth)/2;
-            }
+            alert("GAME OVER");
+            document.location.reload();
         }
     }
+    
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
@@ -166,7 +136,9 @@ function draw() {
     
     x += dx;
     y += dy;
-    requestAnimationFrame(draw);
 }
 
-draw();
+
+drawScore();
+drawVidas();
+setInterval(draw, 10);
