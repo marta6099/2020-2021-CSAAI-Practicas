ea57f470f7552851048fc4d1fcc1ee72c417f6e1
marta6099 <m.garridoj.2018@alumnos.urjc.es>
2021-05-18 11:03:00 +0200

    Cambios

--- P5/video.js
+++ P5/video.js
@@ -2,19 +2,23 @@
 let play = document.getElementById("play");
 const video1 = document.getElementById("video1");
 const video2 = document.getElementById("video2");
+const video3 = document.getElementById("video3");
 const boton_video1 = document.getElementById("boton_video1");
 const boton_video2 = document.getElementById("boton_video2");
+const boton_video3 = document.getElementById("boton_video3");
 const prueba = document.getElementById("prueba");
 const on = document.getElementById("ON");
 const off = document.getElementById("OFF");
 
 //-- Establecemos las dimensiones de los vídeos
 play.width=600;
-play.height=400;
-video1.width=350;  
-video1.height=200;
-video2.width=350;
-video2.height=200; 
+play.height=600;
+video1.width=400;  
+video1.height=400;
+video2.width=400;
+video2.height=400;
+video3.width = 400;
+video3.height = 400; 
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "noseñal.jpg";
@@ -24,23 +28,28 @@ const TEST_IMAGE_URL = "noseñal.jpg";
 play.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 on.onclick = () => {
  
   //-- Establecer la fuente de la cámara 1
-  video1.src="https://youtu.be/7rxwEfFMbaY";
+  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video2.src="https://drive.google.com/uc?export=download&id=1IK4d1sMcp-en3W0eTYnV4rA74bAah1Sj";
+  video3.src ="https://github.com/marta6099/2020-2021-CSAAI-Practicas/blob/main/P5/video1.mp4";
 
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video2.currentTime = 0;
+  video3.currentTime = 0;
   video1.play();
+  video3.play();
   video2.play();
 
   //-- Y en silencio...
   video1.muted =true;
   video2.muted =true;
+  video3.muted = true;
 
   //-- En la emisión en directo ponemos la imagen de prueba
   play.poster = TEST_IMAGE_URL;
@@ -50,23 +59,25 @@ off.onclick = () => {
 
 video1.stop = true;
 video2.stop = true;
+video3.stop = true;
 
 play.poster = TEST_IMAGE_URL;
 
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 video1.src = null;
 video2.src = null;
+video3.src = null;
 play.src = null;
 
 };
 
-
 //-- Botón de Test
 prueba.onclick = () => {
-    directo.poster = TEST_IMAGE_URL;
-    directo.src = null;
+    play.poster = TEST_IMAGE_URL;
+    play.src = null;
 };
 
 //-- Botón de Selección de la cámara 1
@@ -80,6 +91,13 @@ boton_video1.onclick = () => {
 boton_video2.onclick = () => {
     play.src = video2.src;
     play.currentTime = video2.currentTime;
+    play.play();
+    play.poster=null;
+};
+
+boton_video3.onclick = () => {
+    play.src = video3.src;
+    play.currentTime = video3.currentTime;
     play.play();
     play.poster=null;
 };
